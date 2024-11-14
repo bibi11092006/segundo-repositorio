731ed3030fac72b4c33cb9f43a26354144a45780
bibi11092006 <138732150+bibi11092006@users.noreply.github.com>
2024-11-13 22:32:35 -0300

    20

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,17 +1,17 @@
 criaCartao (
    'Missão',
-   'Saiba qual é a missão de nossa empresa!',
-   'Nossa missão é ...'
+   ' Nossa Misão Saiba qual é a missão de nossa empresa!',
+   'aumentar a alta estima das pessoas deixar elas satisfeitas com seu novo visual'
 )
 
 criaCartao (
    'Visão',
-   'Saiba qual é a visão de nossa empresa!',
-   'Nossa visão é ...'
+   ' Nossa visão Saiba qual é a visão de nossa empresa!',
+   'garantir a satisfação do cliente proporcional os melhores produtos e serviços de confiança,superando minhas expectativas.'
 )
 
 criaCartao (
    'Valores',
-   'Saiba quais são os valores de nossa empresa!',
-   'Nossos valores são...'
+   'Nossos Valores Saiba quais são os valores de nossa empresa!',
+   'atender todos os clientes com respeito e profissionalismo para atingir resultados esperados por eles.'
 )
